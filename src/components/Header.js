fb86f243816bb0b4ea0da3d5e0ae1cf233bb1638
Sidharth Surya <imsidharthsurya@gmail.com>
2024-05-12 22:35:10 +0530

    image cover property

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ const Header=()=>{
 
     return (
         <div className="mt-4">
-            <img alt="jpl-logo"  className="h-28 lg:mx-[25%]" src={jpllogo}/>
+            <img alt="jpl-logo"  className="object-cover h-28 lg:mx-[25%]" src={jpllogo}/>
         </div>
     )
 }
