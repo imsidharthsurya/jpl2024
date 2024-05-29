5159f4936e2b8699e8b523cae607fdd2301cf6f0
Sidharth Surya <imsidharthsurya@gmail.com>
2024-05-29 10:46:02 +0530

    Auction date added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Comingsoon from './components/Comingsoon';
 import TeamList from './components/TeamList';
 import PlayerList from './components/PlayerList';
 import Footer from './components/Footer';
+import TextScroller from './components/TextScroller';
 const App = () => {
   
 
   return (
     <div>
       <Header/>
+      <TextScroller text="Auction on 4th May, 2024 at Mukhiya Ji Hall from 6:00 PM onwards." />
       <Comingsoon/>
       <TeamList/>
       <PlayerList/>
---
+++ src/components/TextScroller.js
@@ -0,0 +1,55 @@
+// import React, { useState } from "react";
+// import { useSpring, animated } from "react-spring";
+
+// const TextScroller = ({ text }) => {
+//   const [key, setKey] = useState(1);
+
+//   const scrolling = useSpring({
+//     from: { transform: "translate(60%,0)" },
+//     to: { transform: "translate(-60%,0)" },
+//     config: { duration: 10000 },
+//     reset: true,
+//     //reverse: key % 2 == 0,
+//     onRest: () => {
+//       setKey(key + 1);
+//     }
+//   });
+
+//   return (
+//     <div key={key}>
+//       <animated.div style={scrolling}>{text}</animated.div>;
+//     </div>
+//   );
+// };
+
+// export default TextScroller;
+
+
+
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
+import "./TextScroller.css"; // Import the CSS file for additional styles
+
+const TextScroller = ({ text }) => {
+  const [key, setKey] = useState(1);
+
+  const scrolling = useSpring({
+    from: { transform: "translate(100%, 0)" },
+    to: { transform: "translate(-100%, 0)" },
+    config: { duration: 10000 },
+    reset: true,
+    onRest: () => {
+      setKey(key + 1);
+    }
+  });
+
+  return (
+    <div className="text-scroller-container my-10 text-xl text-red-400 font-bold" key={key}>
+      <animated.div className="text-scroller-content" style={scrolling}>
+        {text}
+      </animated.div>
+    </div>
+  );
+};
+
+export default TextScroller;
